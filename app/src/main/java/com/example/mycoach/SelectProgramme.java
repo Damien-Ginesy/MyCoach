564b8c1bcab88295fe28ccddbcb2f9a471acd82f
Damien <dginesy@enseirb-matmeca.fr>
2023-06-09 17:56:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mycoach/SelectProgramme.java
@@ -0,0 +1,13 @@
+package com.example.mycoach;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectProgramme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_programme);
+    }
+}
